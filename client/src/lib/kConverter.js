3fe576de5fe19fb6df2a105be778c38b0fc34bc9
Anima <tech.is.fashion30@gmail.com>
2026-04-23 00:56:54 +0530

    first commit

---
+++ client/src/lib/kConverter.js
@@ -0,0 +1,8 @@
+// K coverter
+export const kConverter =(num)=>{
+  if (num >=1000){
+    return (num/1000).toFixed(1) + "k"
+  }else {
+    return num
+  }
+  }
---
+++ client/src/lib/timeFormat.js
@@ -0,0 +1,8 @@
+
+const timeFormat=(minutes)=>{
+    const hours = Math.floor(minutes / 60);
+    const minutesReainder = minutes % 60;
+    return `${hours}h ${minutesReainder}m`
+}
+
+export default timeFormat;
